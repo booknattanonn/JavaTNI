58e5c4800ea15c6e7f593dfc5a02ef9b15845fa8
booknattanonn <nongbook0909@gmail.com>
2025-11-18 13:55:44 +0700

    submit exercise Labsheet1 ส่งเเบบฝึกหัดข้อ 1,2,5

---
+++ Lab101.java
@@ -0,0 +1,9 @@
+public class Lab101 {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Nattanon Maneesirimongkol");
+       // System.out.println("\t\tThis is my first time learning Java");
+        System.out.println("\t\tThis is my first time learning Java");
+        System.out.println("\t\tT hope to get grade \"A\"");
+
+    }
+}
---
+++ Lab105.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Lab105 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Summation number = " + (2+5) +
+                "\nSubtraction number = " + (2 - 5)+ "\nMultiplication number = " + (2 * 5) + "\nDission number = " + (2/5.0)+
+                "\nModoulus number = " + (2%5), "Arithmetic Operators", JOptionPane.WARNING_MESSAGE);
+
+    }
+}
